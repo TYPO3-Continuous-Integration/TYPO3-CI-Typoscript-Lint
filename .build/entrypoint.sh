aa94f1c2e0c1f01c924b9ab33c9ca05c7d3a2762
Łukasz Uznański <l.uznanski@macopedia.pl>
2021-03-20 18:13:01 +0100

    Update entrypoint

--- .build/entrypoint.sh
+++ .build/entrypoint.sh
@@ -7,10 +7,6 @@ Header() {
   ##########
   echo "---------------------------------------------"
   echo "-- GitHub Actions TYPO3 Typoscript Linter ---"
-  echo " - Image Creation Date:[${BUILD_DATE}]"
-  echo " - Image Revision:[${BUILD_REVISION}]"
-  echo " - Image Version:[${BUILD_VERSION}]"
-  echo "---------------------------------------------"
   echo "---------------------------------------------"
   echo "TYPO3 Typoscript Linter source code can be found at:"
   echo " - https://github.com/TYPO3-Continuous-Integration/TYPO3-CI-Typoscript-Lint"
@@ -25,7 +21,7 @@ Lint() {
   echo "---------------------------------------------"
   echo "-- Running lint... ---"
   echo "---------------------------------------------"
-  typoscript-lint -c TsLint.yml --ansi -n --fail-on-warnings -vvv ./Configuration/TypoScript
+  typoscript-lint -c /TsLint.yml --ansi -n --fail-on-warnings -vvv ./Configuration/TypoScript
 }
 
 #### Function Footer ###########################################################
